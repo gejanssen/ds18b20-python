aab88fde56306fd58dfe275cc31da405a656fea9
gejanssen <github@gejanssen.com>
2016-09-07 17:35:06 +0000

    foutje in script

--- gettemp.sh
+++ gettemp.sh
@@ -1,1 +1,2 @@
-sudo /usr/bin/python /home/gej/ds18b20/ds18b20/get1temp.py 28-0215635672ff
+#sudo /usr/bin/python /home/gej/ds18b20/ds18b20/get1temp.py 28-0215635672ff
+sudo /usr/bin/python get1temp.py 28-0215635672ff
